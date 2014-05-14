f2c7bb59251cf9134551814ff6eefb7dfc8adec8
zhuchen <zc2027208@sina.com>
2014-05-14 14:25:24 +0800

    add screenshot and refactor demo

--- src/com/appscumen/example/AndroidSwitchDemoActivity.java
+++ src/com/appscumen/example/AndroidSwitchDemoActivity.java
@@ -1,44 +1,26 @@
 package com.appscumen.example;
 
-import com.appscumen.example.MySwitch.OnChangeAttemptListener;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.TextView;
 
-public class AndroidSwitchDemoActivity extends Activity implements OnChangeAttemptListener, OnCheckedChangeListener {
+public class AndroidSwitchDemoActivity extends Activity implements OnCheckedChangeListener {
+
 	MySwitch slideToUnLock;
-//	MySwitch publishToggle;
-//	TextView yes, no;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-       /* slideToUnLock = (MySwitch)findViewById(R.id.switch3);
-        publishToggle = (MySwitch)findViewById(R.id.switch4);
-        yes = (TextView)findViewById(R.id.Yes);
-        no = (TextView)findViewById(R.id.No);*/
-//        slideToUnLock.toggle();
-//        slideToUnLock.disableClick();
-//        slideToUnLock.fixate(true);
-//        publishToggle.setOnCheckedChangeListener(this);
-//		yes.setEnabled(false);
-//		no.setEnabled(true);
-		//no.setTextScaleX(1.2f);
-//        slideToUnLock = (MySwitch)findViewById(R.id.switch1);
-//        slideToUnLock.setOnChangeAttemptListener(this);
-//        slideToUnLock.setOnCheckedChangeListener(this);
-    }
+
 	@Override
-	public void onChangeAttempted(boolean isChecked) {
-		Log.i("xxxx", "isChecked="+isChecked);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		slideToUnLock = (MySwitch) findViewById(R.id.switch1);
+		slideToUnLock.setOnCheckedChangeListener(this);
 	}
+
 	@Override
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		Log.i("xxxx", "isChecked="+isChecked+",buttonView="+buttonView);
+		Log.i("xxxx", "onCheckedChanged isChecked=" + isChecked + ",buttonView=" + buttonView);
 	}
 }
